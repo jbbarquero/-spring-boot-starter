78324e89e974811dfe622cf80c0e7b34426dd458
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-03-27 13:15:13 +0100

    First commit: https://spring.io/blog/2015/03/18/spring-boot-support-in-spring-tool-suite-3-6-4

---
+++ src/main/java/com/malsolo/springframework/boot/starter/HelloProperties.java
@@ -0,0 +1,23 @@
+package com.malsolo.springframework.boot.starter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("hello")
+public class HelloProperties {
+
+	/**
+	 * Greeting message returned by the Hello Rest service.
+	 */
+	private String greeting = "Welcome ";
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+}
